efca381e48a4bf283f140b66adc4333ffbc3e4ca
Artem <artemka86ber@gmail.com>
2022-02-03 19:42:59 +0500

    feat popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
-const popup = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__edit')
-const closeButton = document.querySelector('.popup__close-button')
-
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit');
+let closeButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name');
+let profileInfo = document.querySelector('.profile__description');
+let popupEditName = document.querySelector('.popup__part_name');
+let popupEditDescription = document.querySelector('.popup__part_description');
+let formElement = document.querySelector('.popup__form');
 
 function openEdit() {
-  // evt.preventDefault();
   popup.classList.add('popup__opened');
 }
 
@@ -12,7 +15,23 @@ function closeEdit() {
   popup.classList.remove('popup__opened');
 }
 
-
+// Открытие попапа
 editButton.addEventListener('click', openEdit);
 closeButton.addEventListener('click', closeEdit);
 
+// Загрузка текущих данных в попап
+popupEditName.value = profileName.textContent;
+popupEditDescription.value = profileInfo.textContent;
+
+
+
+// Функция изменения именя и информации о себе
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = popupEditName.value;
+    profileInfo.textContent = popupEditDescription.value;
+    closeEdit()
+}
+
+// Следим за кнопкой отправить
+formElement.addEventListener('submit', formSubmitHandler);
