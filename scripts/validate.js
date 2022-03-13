cd9110dfde79cea972b6a2bb682f80b2fe1706b3
Artem <artemka86ber@gmail.com>
2022-03-14 01:45:44 +0500

    feature togglebutton

--- scripts/validate.js
+++ scripts/validate.js
@@ -23,14 +23,17 @@ const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__edit-input'));
   const buttonElement = formElement.querySelector('.popup__save');
 
+  toggleButtonState(inputList, buttonElement);
+
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', (evt) => {
 
-      isValid(formElement, inputElement)
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
 
     });
   });
-  toggleButtonState(inputList, buttonElement)
+
 };
 
 
@@ -65,8 +68,10 @@ const hideError = (formElement, inputElement) => {
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
     buttonElement.classList.add('popup__save_inactive');
+    buttonElement.setAttribute('disabled', true);
   } else {
     buttonElement.classList.remove('popup__save_inactive');
+    buttonElement.removeAttribute('disabled');
   }
 };
 
