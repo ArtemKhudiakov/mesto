f4710dc49fb0c04ccef57270d60e6bcbf8634dc1
Artem <artemka86ber@gmail.com>
2022-04-24 17:51:20 +0500

    refactor elementlike

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,6 +10,7 @@ export class Card {
     this._cardElement = this._cardTemplate.querySelector('.element').cloneNode(true);
     this._cardImage = this._cardElement.querySelector('.element__image');
     const cardTitle = this._cardElement.querySelector('.element__title');
+    this._elementLike = this._cardElement.querySelector('.element__like');
 
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
@@ -22,8 +23,7 @@ export class Card {
 
   // Сделать лайк
   _handleLikeClick = () => {
-    const elementLike = this._cardElement.querySelector('.element__like');
-    elementLike.classList.toggle('element__like_active');
+    this._elementLike.classList.toggle('element__like_active');
   }
   // Удалить карточку
   _handleDeleteCard = () => {
@@ -32,7 +32,7 @@ export class Card {
 
   //Повесить слушателей
   _setEventListeners() {
-    this._cardElement.querySelector('.element__like').addEventListener('click', this._handleLikeClick);
+    this._elementLike.addEventListener('click', this._handleLikeClick);
     this._cardElement.querySelector('.element__trash').addEventListener('click', this._handleDeleteCard);
     this._cardImage.addEventListener('click', () => {this._handleCardClick(this._name, this._link)});
   }
