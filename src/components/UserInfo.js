caaa15cdfa6773769c9236478db6be773eb43446
Artem <artemka86ber@gmail.com>
2022-05-07 17:05:09 +0500

    ready get infos from server

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,8 +1,9 @@
 
 export class UserInfo {
-constructor({nameSelector, infoSelector}) {
+constructor({nameSelector, infoSelector, avatarSelector}) {
     this._name = document.querySelector(nameSelector);
     this._info = document.querySelector(infoSelector);
+    this._userAvatar = document.querySelector(avatarSelector);
 
   }
 
@@ -15,7 +16,14 @@ constructor({nameSelector, infoSelector}) {
 
   setUserInfo(userData) {
     this._name.textContent = userData.name;
-    this._info.textContent = userData.info;
+    this._info.textContent = userData.about;
+  }
+
+  getUserAvatar() {
+    return this._userAvatar.src
+  }
+  setUserAvatar(data) {
+    this._userAvatar.src = data.avatar;
   }
 
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import "./index.css"
 
-import { initialCards } from "../utils/cards.js"
+// import { initialCards } from "../utils/cards.js"
 import { FormValidator } from "../components/FormValidator.js";
 import { Card } from "../components/Card.js"
 import { Section } from '../components/Section.js';
 import { PopupWithForm } from '../components/PopupWithForm.js';
 import { PopupWithImage } from '../components/PopupWithImage.js';
 import { UserInfo } from '../components/UserInfo.js';
+import { Api } from '../components/Api.js';
 
 import { validationSettings,
   buttonEditForm,
@@ -22,6 +23,8 @@ import { validationSettings,
   elements,
 } from '../utils/constants.js';
 
+let userId = null
+
 // Валидация
 const profileFormValidation = new FormValidator(validationSettings, buttonEditForm)
 const placeFormValidation = new FormValidator(validationSettings, buttonPlaceForm)
@@ -33,7 +36,39 @@ placeFormValidation.enableValidation();
 const userInfo = new UserInfo ({
   nameSelector: '.profile__name',
   infoSelector: '.profile__description',
+  avatarSelector: '.profile__avatar',
 });
+
+
+// API
+//
+
+const api = new Api({
+  url: 'https://mesto.nomoreparties.co/v1/cohort-40',
+  headers: {
+    authorization: '6a3dc8fc-cbc8-4cee-b5dd-4c0b0fbbc466',
+    'Content-Type': 'application/json'
+  }
+});
+
+
+const userServerInfo = api.getUserInfoServer();
+console.log(userServerInfo);
+const initialServerCards = api.getInitialCards();
+console.log(initialServerCards);
+
+Promise.all([userServerInfo, initialServerCards])
+   .then(([userData, initialCards]) => {
+      userInfo.setUserInfo(userData);
+      userInfo.setUserAvatar(userData);
+      userId = userData._id;
+      console.log(userId);
+      cardsSection.renderAll(initialCards);
+      })
+   .catch((err) => console.log(`Ошибка загрузки данных: ${err}`));
+
+
+
 
 // Создание попапа редактирования профиля
 const typePopupEditProfile = new PopupWithForm(popupEdit,
@@ -66,14 +101,14 @@ function makeCard(item) {
 
 // Секция карточек
 const cardsSection = new Section({
-  items: initialCards,
+  // items: initialCards,
   renderer: (item) => {
     const card = makeCard(item);
     cardsSection.addItem(card);
   }
 }, elements);
 
-cardsSection.renderAll();
+// cardsSection.renderAll();
 
 const typePopupImage = new PopupWithImage(popupImage);
 
@@ -101,3 +136,5 @@ typePopupEditProfile.setEventListeners();
 // Слушатели на кнопки
 editButton.addEventListener('click', openEditProfile);
 buttonAddPlace.addEventListener('click', createNewPlace);
+
+
